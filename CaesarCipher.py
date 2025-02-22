a7354570632bd1f1124c3a37a734e5b244505307
1oetkenjdo <jacoboetken@unomaha.edu>
2025-02-22 22:23:53 +0000

    Lab Complete. Please see wordgame for a message/question at the end. Along with the WORDGAMEUPDATE file with the code that is discussed

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,8 +1,7 @@
-#Caesar Cipher
-#The Caesar cipher moves each letter forward in the alphabet by
-#the key.  The resulting message has all the letters advanced by 'key'
-#letters.
-#To run the code, run the main() function
+#CeasarCipher.py
+#Name: Jacob Oetken
+#Date: 2/22/2025
+#Assignment: LAB 5
 
 def encode(message, key):
     alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -10,26 +9,44 @@ def encode(message, key):
     secret = ""
 
     for letter in message:
-        if (alpha.find(letter) >= 0): #check to see if the letter is actually a letter
+        if (alpha.find(letter) >= 0): #letter is letter
             spot = (alpha.find(letter) + key) % 26
             secret = secret + alpha[spot]
-        else: # letter must have been a number, symbol, or punctuation.
+        else: #letter is not letter
             secret = secret + letter
 
     return secret
 
-#def decode(message, key):
-    #We will want to decode the message here.
+def decode(message, key):
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    message = message.upper()
+    decoded_message = ""
+
+    for letter in message:
+        if (alpha.find(letter) >= 0): #letter is letter
+            spot = (alpha.find(letter) - key) % 26
+            decoded_message = decoded_message + alpha[spot]
+        else: #letter is not letter
+            decoded_message = decoded_message + letter
+
+    return decoded_message
+
+
+
 
 def main():
     message = input("Enter a message: ")
     key = int(input("Enter a key: "))
 
     secret = encode(message, key)
-    print ("Encrypted:", secret)
-    #plaintext = decode(secret, key)
-    #print ("Decrypted:", plaintext)
-
+    print("Encrypted:", secret)
+    
+    decode_response = input("Do you want to decode the message? (Y/N): ").strip().lower() 
+    if decode_response == 'y':
+        plaintext = decode(secret, key)
+        print("Decrypted:", plaintext)
+    else:
+        print("Decoding skipped.")
 
 if __name__ == '__main__':
-  main()
+    main()
---
+++ WORDGAMEUPDATE.py
@@ -0,0 +1,76 @@
+#this file is just the code from the end of my original wordgame file.
+
+#above is the original code that i wrote, however when i had my wife play it i noticed that the code would say something like #AlaE where its     "#" unknown/wrong letter | A | L wrong spot | A wrong spot | E     if the word was SCALE. 
+#meaning if i had a correct letter, but in the wrong spot, and the correct letter in the right spot, it was still telling me that there was another of that letter. (if that makes sense)
+#i sent what i had off to my buddy who is *only slightly* (or a lot) smarter than me with programming, and what is below is what he sent me back and what we came up with, which doesnt have that problem, but isnt my work and i am not 100% sure what/how he did it.
+#is there a way for you to kind of describe what he did in more detail? maybe zoom or something?!
+
+import random
+
+def inWord(letter, word):
+     """Returns boolean if letter is anywhere in the given word"""
+     return letter in word
+
+def inSpot(letter, word, spot):
+     """Returns boolean response if letter is in the given spot in the word."""
+     return word[spot] == letter
+
+def rateGuess(myGuess, word):
+     """Rates your guess and returns a word with the following features.
+     - Capital letter if the letter is in the right spot
+     - Lower case letter if the letter is in the word but in the wrong spot
+     - * if the letter is not in the word at all"""
+     result = ["*"] * len(myGuess)
+     word_list = list(word)
+
+     # First pass: mark correct letters in the correct spot
+     for i in range(len(myGuess)):
+         if inSpot(myGuess[i], word, i):
+             result[i] = myGuess[i].upper()
+             word_list[i] = None  # Mark this letter as used
+
+     # Second pass: mark correct letters in the wrong spot
+     for i in range(len(myGuess)):
+         if result[i] == "*" and myGuess[i] in word_list:
+             result[i] = myGuess[i].lower()
+             word_list[word_list.index(myGuess[i])] = None  # Mark this letter as used
+
+     return "".join(result)
+
+def main():
+     #Pick a random word from the list of all words
+     wordFile = open("words.txt", 'r')
+     content = wordFile.read()
+     wordList = content.split("\n")
+     theWord = random.choice(wordList).strip()
+    
+     #User should get 6 guesses to guess
+     attempts = 6
+
+     print("Guess the 5-letter word!")
+
+     while attempts > 0:
+         #Ask user for their guess
+         while True:
+             myGuess = input("Enter your guess: ").strip().lower()
+             # Check if the guess is a 5-letter word
+             if len(myGuess) == 5:
+                 break
+             print("Please enter a 5-letter word.")
+
+         #Give feedback using on their word
+         feedback = rateGuess(myGuess, theWord)
+         print("Feedback:", feedback)
+
+         if myGuess == theWord:
+             print("Congratulations! You've guessed the word correctly.")
+             break
+
+         attempts -= 1
+         print(f"You have {attempts} attempts left.")
+
+     if attempts == 0:
+         print(f"Sorry, you've run out of attempts. The word was: {theWord}")
+
+if __name__ == '__main__':
+     main()
